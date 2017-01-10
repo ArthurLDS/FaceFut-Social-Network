cf8e85573524a68be534d2566bc61136b9026e02
ArthurLDS <arthurlds73@gmail.com>
2017-01-10 17:33:45 -0200

    feat(pesquisaJS): implementar iserção de partial View a partir de uma requisição GET com AJAX, para renderizar os resultados da pesquisa.

--- social/social-web/src/main/resources/static/js/pesquisa.js
+++ social/social-web/src/main/resources/static/js/pesquisa.js
@@ -1,10 +1,31 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-$(document).ready(
-    function(){
-    }
-)
+/**
+@Autor: Arthur Lima de Souza
+ **/
+
+var pesquisa = [];
+
+$(function(){
+    pesquisa.configurarBotoes();
+   }
+);
+
+pesquisa.configurarBotoes = function(){
+    pesquisa.$btnPesquisar =  $('#btn-pesquisar-amigo');
+    pesquisa.$btnPesquisar.click(pesquisa.pesquisar);
+};
+
+pesquisa.pesquisar = function(){
+    let filtro = $('#txt-pesquisa-amigo').val();
+    
+    $.get("/pesquisa/pesquisarAmigo", {filtro})
+        .then(function(response){
+                pesquisa.renderizarResultados(response);
+            }
+        )
+    
+};
+
+pesquisa.renderizarResultados = function(response){
+    $('#box-pesquisaDeAmigos').html(response);
+}
 
