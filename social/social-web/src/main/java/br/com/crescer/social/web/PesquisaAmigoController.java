fcb6c5365bc06eb1a64e2ce6ea252a807dc21fdd
ArthurLDS <arthurlds73@gmail.com>
2017-01-12 21:50:57 -0200

    feat(PesquisaAmigoController): implementar método para filtrar listagem da pesquisa.

--- social/social-web/src/main/java/br/com/crescer/social/web/PesquisaAmigoController.java
+++ social/social-web/src/main/java/br/com/crescer/social/web/PesquisaAmigoController.java
@@ -6,9 +6,14 @@
 package br.com.crescer.social.web;
 
 import br.com.crescer.social.entity.Amigo;
+import br.com.crescer.social.entity.Usuario;
 import br.com.crescer.social.service.Service.AmigoService;
 import br.com.crescer.social.service.Service.UsuarioService;
+import java.util.ArrayList;
+import org.springframework.security.core.userdetails.User;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,16 +24,50 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author Arthur
  */
 @Controller
-@RequestMapping(value="pesquisa")
+@RequestMapping(value = "pesquisa")
 public class PesquisaAmigoController {
-    
+
     @Autowired
     AmigoService amigoService;
-    
-    @RequestMapping(value="pesquisarAmigo", method = RequestMethod.GET)
-    public String pesquisarAmigo(Model model, String filtro){
-        Iterable<Amigo> amigos = amigoService.findByNome(filtro);
+    @Autowired
+    UsuarioService usuarioService;
+
+    @RequestMapping(value = "pesquisarAmigo", method = RequestMethod.GET)
+    public String pesquisarAmigo(Model model, String filtro) {
+        User userSessao = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Usuario usuario = usuarioService.findByEmail(userSessao.getUsername());
+        
+        Iterable<Amigo> amigos = filtrarListaDeAmigos((List) amigoService.findByNomeIgnoreCaseContaining(filtro), usuario);
         model.addAttribute("amigos", amigos);
         return "partialPesquisarAmigos";
+    }
+    
+    // Refatorar isto.
+    private List<Amigo> filtrarListaDeAmigos(List<Amigo> amigos, Usuario usuario) {
+        List<String> nomeAmigos = new ArrayList<>();
+        List<Amigo> amigosDoUsuario = usuario.getAmigos();
+        List<Amigo> amigosFiltrados = amigos;
+
+        for (int i = 0; i < amigosDoUsuario.size(); i++) {
+            nomeAmigos.add(amigosDoUsuario.get(i).getEmail());
+        }
+
+        for (int i = 0; i < amigos.size(); i++) {
+            Amigo amigoAtual = amigos.get(i);
+            if (amigosDoUsuario.size() > 0) {
+                for (int j = 0; j < nomeAmigos.size(); j++) {
+                    if (amigoAtual.getEmail().equals(nomeAmigos.get(j))) {
+                        amigosFiltrados.remove(amigoAtual);
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < amigosFiltrados.size(); i++) {
+            Amigo amigoAtual = amigos.get(i);
+            if (amigoAtual.getEmail().equals(usuario.getEmail())) {
+                amigosFiltrados.remove(amigoAtual);
+            }
+        }
+        return amigosFiltrados;
     }
 }
