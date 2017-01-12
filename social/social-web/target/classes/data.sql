176946a5c7145f4c0fd92a785c99e7ea84cc285c
ArthurLDS <arthurlds73@gmail.com>
2017-01-12 21:51:49 -0200

    fix(DataSQL): corrigir comandos SQL,

--- social/social-web/target/classes/data.sql
+++ social/social-web/target/classes/data.sql
@@ -6,29 +6,29 @@ INSERT INTO TIME (ID_TIME, NM_TIME) VALUES(4, 'Juventude');
 
 -- Usuários --
 INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, TIME_ID_TIME) 
-VALUES(5, 'arthurlds73@gmail.com', 'Arthur Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+VALUES(1000, 'arthurlds73@gmail.com', 'Arthur Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
 
 INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, TIME_ID_TIME) 
-VALUES(6, 'rafaeldesouza@gmail.com', 'Rafael de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+VALUES(1001, 'rafaeldesouza@gmail.com', 'Rafael de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
 
 INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, TIME_ID_TIME) 
-VALUES(7, 'maralima@gmail.com', 'Mara Regina Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+VALUES(1002, 'maralima@gmail.com', 'Mara Regina Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
 
 INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, TIME_ID_TIME) 
-VALUES(8, 'larridesouza@gmail.com', 'Larri Angeli de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+VALUES(1003, 'larridesouza@gmail.com', 'Larri Angeli de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
 
 -- Amigos -- 
 INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, TIME_ID_TIME) 
-VALUES(5, 'Arthur Lima de Souza', 'arthurlds73@gmail.com', 1);
+VALUES(1000, 'Arthur Lima de Souza', 'arthurlds73@gmail.com', 1);
 
 INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, TIME_ID_TIME) 
-VALUES(6, 'Rafael de Souza', 'rafaeldesouza@gmail.com', 1);
+VALUES(1001, 'Rafael de Souza', 'rafaeldesouza@gmail.com', 1);
 
 INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, TIME_ID_TIME) 
-VALUES(7, 'Mara Regina Lima de Souza', 'maralima@gmail.com', 1);
+VALUES(1002, 'Mara Regina Lima de Souza', 'maralima@gmail.com', 1);
 
 INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, TIME_ID_TIME) 
-VALUES(8, 'Larri Angeli de Souza', 'larridesouza@gmail.com', 1);
+VALUES(1003, 'Larri Angeli de Souza', 'larridesouza@gmail.com', 1);
 
 
 
