3fe4c3c0d4bb9987e52c69b5051327415cfb5991
ArthurLDS <arthurlds73@gmail.com>
2017-01-13 17:21:45 -0200

    feat(amigoJS): implementar carregamento de lista de amigos e atulização de contagem de amigos na View.

--- social/social-web/src/main/resources/static/js/amigo.js
+++ social/social-web/src/main/resources/static/js/amigo.js
@@ -4,11 +4,47 @@
 
 var amigo = {};
 
+$(function(){
+    amigo.carregarListaAmigos();
+    amigo.atualizarNumeroDeAmigos();
+    setInterval(
+        function(){
+            amigo.carregarListaAmigos();
+            amigo.atualizarNumeroDeAmigos();
+        }, 5000);
+});
+
 amigo.enviarConvite = function(id){
     
-  $.get('/amigo/enviarConvite', {id})
+  $.get('/amigoRest/enviarConvite', {id})
           .then(function(){
-              alert('Foii!!');
+              amigo.desabilitarBtnAdicionar();
           });
 };
+
+amigo.desabilitarBtnAdicionar = function(){
+    $btnAdicionar = $('.btn-adicionar');
+    
+    $btnAdicionar.attr('disabled','disabled');
+    $btnAdicionar.removeClass('btn-primary');
+    $btnAdicionar.addClass('btn-success');
+    $btnAdicionar.html('<i class="fa fa-check" aria-hidden="true"></i> Convite enviado');
+}
+
+
+amigo.carregarListaAmigos = function(){
+    
+    $.get("/amigo/carregarListaAmigos")
+            .then(function(response){
+                $('#box-lista-amigos').html(response);
+            });
+};
+
+amigo.atualizarNumeroDeAmigos = function(){
+    
+    $.get("/amigoRest/atualizarNumAmigos")
+            .then(function(response){
+                 $('#spam-num-amigos').text(response);
+            });
+}
 
