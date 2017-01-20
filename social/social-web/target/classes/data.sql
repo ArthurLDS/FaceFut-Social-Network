6ef4645d2501f1c7e2c4ab3e33b676a293ad9be1
ArthurLDS <arthurlds73@gmail.com>
2017-01-20 00:19:50 -0200

    feat(DataSQL): implementar inserção de dados de perfil no banco de dados.

--- social/social-web/target/classes/data.sql
+++ social/social-web/target/classes/data.sql
@@ -4,31 +4,44 @@ INSERT INTO TIME (ID_TIME, NM_TIME) VALUES(2, 'Gremio');
 INSERT INTO TIME (ID_TIME, NM_TIME) VALUES(3, 'Brasil de Pelotas');
 INSERT INTO TIME (ID_TIME, NM_TIME) VALUES(4, 'Juventude');
 
+-- Perfis --
+INSERT INTO PERFIL(ID_PERFIL, IMAGEM_PERFIL)
+VALUES(1000, '/imgs/01-cnorris.png');
+
+INSERT INTO PERFIL(ID_PERFIL, IMAGEM_PERFIL)
+VALUES(1001, '/imgs/chaves.jpg');
+
+INSERT INTO PERFIL(ID_PERFIL, IMAGEM_PERFIL)
+VALUES(1002, '/imgs/donaflorinda.png');
+
+INSERT INTO PERFIL(ID_PERFIL, IMAGEM_PERFIL)
+VALUES(1003, '/imgs/seumadruga.jpg');
+
 -- Usuários --
-INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME) 
-VALUES(1000, 'arthurlds73@gmail.com', 'Arthur Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME, ID_PERFIL) 
+VALUES(1000, 'arthurlds73@gmail.com', 'Arthur Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1, 1000);
 
-INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME) 
-VALUES(1001, 'rafaeldesouza@gmail.com', 'Rafael de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME, ID_PERFIL) 
+VALUES(1001, 'rafaeldesouza@gmail.com', 'Rafael de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1, 1001);
 
-INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME) 
-VALUES(1002, 'maralima@gmail.com', 'Mara Regina Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME, ID_PERFIL) 
+VALUES(1002, 'maralima@gmail.com', 'Mara Regina Lima de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1, 1002);
 
-INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME) 
-VALUES(1003, 'larridesouza@gmail.com', 'Larri Angeli de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1);
+INSERT INTO USUARIO (ID_USUARIO, EMAIL, NM_USUARIO, SENHA, ID_TIME, ID_PERFIL) 
+VALUES(1003, 'larridesouza@gmail.com', 'Larri Angeli de Souza', '$2a$10$u1A9pPCNLpUiNwvREkHDOOivSthu28yRmNDddtOtykm6lBFVDD1S2', 1, 1003);
 
 -- Amigos -- 
-INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME) 
-VALUES(1000, 'Arthur Lima de Souza', 'arthurlds73@gmail.com', 1);
+INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME, ID_PERFIL) 
+VALUES(1000, 'Arthur Lima de Souza', 'arthurlds73@gmail.com', 1, 1000);
 
-INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME) 
-VALUES(1001, 'Rafael de Souza', 'rafaeldesouza@gmail.com', 1);
+INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME, ID_PERFIL) 
+VALUES(1001, 'Rafael de Souza', 'rafaeldesouza@gmail.com', 1, 1001);
 
-INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME) 
-VALUES(1002, 'Mara Regina Lima de Souza', 'maralima@gmail.com', 1);
+INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME, ID_PERFIL) 
+VALUES(1002, 'Mara Regina Lima de Souza', 'maralima@gmail.com', 1, 1002);
 
-INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME) 
-VALUES(1003, 'Larri Angeli de Souza', 'larridesouza@gmail.com', 1);
+INSERT INTO AMIGO (ID_AMIGO, NM_AMIGO, EMAIL, ID_TIME, ID_PERFIL) 
+VALUES(1003, 'Larri Angeli de Souza', 'larridesouza@gmail.com', 1, 1003);
 
 
 
