d22ecbb7b1755b3c5c03c678821eabd96ce60f94
ArthurLDS <arthurlds73@gmail.com>
2017-01-26 23:35:45 -0200

    feat(postagemJS): implementar método para fazer post com imagem passando um objeto multipart para a controller.

--- social/social-web/src/main/resources/static/js/postagem.js
+++ social/social-web/src/main/resources/static/js/postagem.js
@@ -4,16 +4,18 @@
  * and open the template in the editor.
  */
 
+/* global file */
+
 var postagem = {};
 
 $(function () {
     postagem.configurarForm();
     postagem.carregarPosts();
-    
+
     //setInterval(function(){
     //    postagem.carregarPosts();
     //}, 5000);
-    
+
 });
 
 postagem.configurarForm = function () {
@@ -24,14 +26,36 @@ postagem.configurarForm = function () {
 };
 
 postagem.postar = function () {
+    let file = new FormData($("#upload-file-form")[0]);
 
-    $.post("/postagemRest/postar", {texto: $txtPost.val()})
-            .then(function () {
+    $.ajax({
+        url: "/postagemRest/uploadImagem",
+        type: "POST",
+        data: file,
+        processData: false,
+        contentType: false,
+        cache: false,
+        success: function (response) {
+            postagem.postagemTexto(response);
+        },
+        error: function () {
+            alert("Erroo!");
+        }
+    });
+};
+
+postagem.postagemTexto = function (response) {
+    $.post("/postagemRest/postar", {caminhoImagem: response, texto: $txtPost.val()})
+            .done(function () {
                 $txtPost.val('');
                 postagem.carregarPosts();
-            }
-            );
-};
+                postagem.carregarPosts();
+            })
+            .fail(function () {
+                alert('ops!');
+            });
+}
+
 
 postagem.carregarPosts = function () {
 
