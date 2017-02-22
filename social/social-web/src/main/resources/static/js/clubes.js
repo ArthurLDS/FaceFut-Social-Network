9f1f88c442141571a8e1b01094abd7ab9cfb87de
ArthurLDS <arthurlds73@gmail.com>
2017-02-22 20:35:24 -0300

    reafactor(clubesJS): alterar nomenclatura de objeto.

--- social/social-web/src/main/resources/static/js/clubes.js
+++ social/social-web/src/main/resources/static/js/clubes.js
@@ -1,35 +1,35 @@
 
-var jogos = {};
+var clube = {};
 
 $(function () {
-    jogos.carregarElementos();
-    jogos.carregarPlacares();
+    clube.carregarElementos();
+    clube.carregarPlacares();
 });
 
-jogos.carregarElementos = function () {
-    jogos.jogosContent = $('#box-jogos');
+clube.carregarElementos = function () {
+    clube.jogosContent = $('#box-jogos');
 }
 
-jogos.carregarPlacares = function () {
+clube.carregarPlacares = function () {
     $.getJSON('https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/en.1.json', function (json) {
         let rounds = json.rounds.map(r => r.matches);
-        jogos.renderizarJogosPorData(rounds);
+        clube.renderizarJogosPorData(rounds);
     });
 };
 
-jogos.renderizarJogosPorData = function(rounds) {
+clube.renderizarJogosPorData = function(rounds) {
     let agora = new Date();
     
     for (var i = 0; i < rounds.length; i++) {
         for (var j = 0; j < rounds[i].length; j++) {
             let mesRodada = rounds[i][j].date.split("-")[1];
             if(mesRodada ==  eval(agora.getMonth() + 1))
-                jogos.jogosContent.append(rounds[i][j].team1.code + " x " + rounds[i][j].team2.code + "<br/>Data: " + rounds[i][j].date + "<br/><br/>");
+                clube.jogosContent.append(rounds[i][j].team1.code + " x " + rounds[i][j].team2.code + "<br/>Data: " + rounds[i][j].date + "<br/><br/>");
         }
     }
 };
 
-jogos.separarJogosPorData = function(){
+clube.separarJogosPorData = function(){
     
 };
 
